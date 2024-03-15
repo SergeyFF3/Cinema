a205b86af5dd1bcc136872b39d40f5f784d6729d
SergeyFF3 <sergey@redrazr.com>
2024-03-15 10:16:43 +0300

    edited VideoPlayer if not trailers

--- src/widgets/VideoPlayer/ui/VideoPlayer.tsx
+++ src/widgets/VideoPlayer/ui/VideoPlayer.tsx
@@ -2,11 +2,17 @@ import { FC } from 'react';
 import YouTube from 'react-youtube';
 import styles from './VideoPlayer.module.css';
 import { ITrailer } from 'src/shared/types';
+import { Typography } from '@mui/material';
 
 export const VideoPlayer: FC<{ trailers: ITrailer[] }> = ({ trailers }) => {
   const videoId = trailers[0].url.match(/embed\/([^/?]+)/)?.[1];
 
-  if (!videoId || !trailers) return null;
+  if (!videoId || !trailers)
+    return (
+      <Typography color="white" fontSize="20px" textAlign="center">
+        Трейлер отсутствует. <br /> Заходите позднее :)
+      </Typography>
+    );
 
   return <YouTube className={styles.videoPlayer} videoId={videoId} />;
 };
